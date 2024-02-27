6e0e442e07c18d13d0a87c525c74e62749c04edb
Davide Buoso <daviibuoso@gmail.com>
2024-02-27 18:42:26 +0100

    train and test (sb3) added

--- train.py
+++ train.py
@@ -7,5 +7,5 @@ from stable_baselines3.common.env_util import make_vec_env
 env = gym.make("LocoMujoco", env_name="UnitreeA1.simple")
 
 model = PPO(policy="MlpPolicy", env=env)
-model.learn(500_000, log_interval=1, progress_bar=True)
+model.learn(1_000_000, log_interval=1, progress_bar=True)
 model.save("final_model")
