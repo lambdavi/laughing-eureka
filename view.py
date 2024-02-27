6ebf09960e9e4bdf5b80c36e05a0778d116ad2cb
Davide Buoso <daviibuoso@gmail.com>
2024-02-27 17:33:42 +0100

    first example with sb3

--- view.py
+++ view.py
@@ -7,7 +7,7 @@ from stable_baselines3.common.env_util import make_vec_env
 #env = make_vec_env((lambda: gym.make("LocoMujoco", env_name="UnitreeA1.simple")), n_envs=2)
 env = gym.make("LocoMujoco", env_name="UnitreeA1.simple")
 
-model = PPO(policy="MlpPolicy", env=env, verbose=1)
+model = PPO(policy="MlpPolicy", env=env, verbose=2)
 model.learn(10_000_000, log_interval=1000)
 
 action_dim = env.action_space.shape[0]
